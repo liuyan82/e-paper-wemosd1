d01065215330743438fcc2b11ceff03afddd2a86
liuyan <liuyan@sogou-inc.com>
2020-04-16 22:26:07 +0800

    copy from waveshare's example and modified for the wemos d1 mini

---
+++ epd4in2/wemosd1_def.h
@@ -0,0 +1,19 @@
+#ifndef _wemosd1_def_h_
+#define _wemosd1_def_h_
+
+#define WE_D0 16
+#define WE_D1 5
+#define WE_D2 4
+#define WE_D3 0
+#define WE_D4 2
+#define WE_D5 14
+#define WE_D6 12
+#define WE_D7 13
+#define WE_D8 15
+
+#define WE_SPI_SCK WE_D5
+#define WE_SPI_MISO WE_D6
+#define WE_SPI_MOSI WE_D7
+#define WE_SPI_SS WE_D8
+
+#endif // _wemosd1_def_h_
